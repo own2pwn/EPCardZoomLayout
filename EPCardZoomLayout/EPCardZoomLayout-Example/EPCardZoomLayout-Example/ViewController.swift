061f457637c15428e4534cb0b4502ba9bc39bbac
own2pwn <evg2own@gmail.com>
2018-03-29 21:09:50 +0300

    Example app project

---
+++ EPCardZoomLayout/EPCardZoomLayout-Example/EPCardZoomLayout-Example/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  EPCardZoomLayout-Example
+//
+//  Created by Evgeniy on 29.03.18.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
